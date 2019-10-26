7fce5040de9cdb52ff51081d060a03ef3dc7b879
juliflorezm <57023939+juliflorezm@users.noreply.github.com>
2019-10-25 22:41:01 -0500

    Update Bibliotecario.java

--- biblioteca/src/main/java/dominio/Bibliotecario.java
+++ biblioteca/src/main/java/dominio/Bibliotecario.java
@@ -29,7 +29,7 @@ public class Bibliotecario {
 		}
 
 		if(esPolindromo(isbn)){
-			throw new PrestamoException("Los libros políndromos solo se pueden utilizar en la biblitoeca");
+			throw new PrestamoException("Los libros polÃ­ndromos solo se pueden utilizar en la biblitoeca");
 		}
 
 		Libro libro = this.repositorioLibro.obtenerPorIsbn(isbn);
@@ -62,21 +62,15 @@ public class Bibliotecario {
 
 	public void prestarEnsayo(String isbn, String nombreUsuario) {
 		if (esPrestado(isbn)) {
-
 			throw new PrestamoException(EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE);
-
-		} else {
-
-			if (esPolindromo(isbn)) {
-				throw new PrestamoException("Los libros políndromos solo se pueden utilizar en la biblitoeca");
-			} else {
-				Libro libro = this.repositorioLibro.obtenerPorIsbn(isbn);
-				Prestamo prestamo = new Prestamo(libro, nombreUsuario);
-				this.repositorioPrestamo.agregar(prestamo);
-
-			}
-
 		}
+
+		if (esPolindromo(isbn)) {
+			throw new PrestamoException("Los libros polÃ­ndromos solo se pueden utilizar en la biblitoeca");
+		} 
+		Libro libro = this.repositorioLibro.obtenerPorIsbn(isbn);
+		Prestamo prestamo = new Prestamo(libro, nombreUsuario);
+		this.repositorioPrestamo.agregar(prestamo);
 	}
 
 	public boolean esMay(String isbn){
@@ -97,30 +91,19 @@ public class Bibliotecario {
 
 	public void prestarEnsayo2(String isbn, String nombreUsuario) {
 		if (esPrestado(isbn)) {
-
 			throw new PrestamoException(EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE);
+		}
 
-		} else {
-
-			if (esPolindromo(isbn)) {
-				throw new PrestamoException("Los libros políndromos solo se pueden utilizar en la biblitoeca");
-			} else {
-				Date fechaSolicitud=new Date();
-				Date fechaEntregaMaxima= null;
-
-				GregorianCalendar fechaCalendario = new GregorianCalendar();
-				fechaCalendario.setTime(fechaSolicitud);
-				int diaSemana = fechaCalendario.get(Calendar.DAY_OF_WEEK);
-
-				if(esMay(isbn)){
-
-				}
-				Libro libro = this.repositorioLibro.obtenerPorIsbn(isbn);
-				Prestamo prestamo = new Prestamo(fechaSolicitud,libro,fechaEntregaMaxima,nombreUsuario);
-				this.repositorioPrestamo.agregar(prestamo);
-			}
+		if (esPolindromo(isbn)) {
+			throw new PrestamoException("Los libros polÃ­ndromos solo se pueden utilizar en la biblitoeca");
+		} 
+		
+		if(esMay(isbn)){
 
 		}
+		Libro libro = this.repositorioLibro.obtenerPorIsbn(isbn);
+		Prestamo prestamo = new Prestamo(fechaSolicitud,libro,fechaEntregaMaxima,nombreUsuario);
+		this.repositorioPrestamo.agregar(prestamo);
 	}
 
 
